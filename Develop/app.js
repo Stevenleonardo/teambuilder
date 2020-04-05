f6ab84730afeac4f1ac7f1df2d45597a17fc8fd6
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-04-05 15:14:27 -0400

    intern inquirer set up

--- Develop/app.js
+++ Develop/app.js
@@ -133,5 +133,84 @@ const manager = () => {
                 //function fullTeam will be started
                 fullTeam();
             }
+            
         })
+        .catch(error => {
+            if (error.isTtyError) {
+                //  Error with prompt
+              } else {
+                // Something else when wrong
+              }
+        });
     }
+        const intern = () =>{
+            //where prompts are placed for intern
+            inquirer
+            .prompt([
+                {
+                    type= "input",
+                    message: "what is your intern's name?",
+                    name: "name",
+
+                    //function to ensure only letters are being used
+                    validate: function(value){
+                        var letters = value.match(/[a-z]/);
+                        //conditionals if requirements are met
+                        if (letters) {
+                            return  true
+                        }
+                        else {
+                            return "Name must only contain letters"
+                        }
+                    }
+                },
+                {
+                    type= "input",
+                    message: "what is your intern's email?",
+                    name: "email",
+
+                    //function to ensure proper email entered
+                    validate: function(value){
+                        var emailInput = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+                        //conditionals if requirements are met
+                        if (emailInput) {
+                            return  true
+                        }
+                        else {
+                            return "Email not entered correctly"
+                        }
+                    }
+                },
+                {
+                    type= "input",
+                    message: "what is your intern's ID?",
+                    name: "id",
+
+                    //function to ensure only numbers are being used
+                    validate: function(value){
+                        var numbers = value.match(/[0-9]/);
+                        //conditionals if requirements are met
+                        if (numbers) {
+                            return  true
+                        }
+                        else {
+                            return "ID must only contain numbers"
+                        }
+                    }
+                },
+                {
+                    type= "input",
+                    message: "what is your intern's school?",
+                    name: "school",
+
+                },
+            ])
+            .then(answers =>{
+                //place answers into a variable
+                var userIntern = new Intern(answers.name, answers.email, answers.id, answers.school);
+                //place into empty array
+                teamRender.push(userIntern);
+                //calls the function newmember
+                newMember();
+            })
+        }
